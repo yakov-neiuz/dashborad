b11a81aae2cbc97ba258e423c3f765b0b14ba318
yakov-neiuz <kchinuch100@gmail.com>
2026-06-15 15:40:09 +0300

    fix: use entity_data endpoint

--- api/leads.js
+++ api/leads.js
@@ -1,7 +1,7 @@
 module.exports = async (req, res) => {
   const { ORIGAMI_BASE_URL, ORIGAMI_USERNAME, ORIGAMI_API_SECRET } = process.env;
   try {
-    const r = await fetch(`${ORIGAMI_BASE_URL}/entities/api/entities_list/format/json`, {
+    const r = await fetch(`${ORIGAMI_BASE_URL}/entities/api/entity_data/format/json`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ username: ORIGAMI_USERNAME, api_secret: ORIGAMI_API_SECRET, entity_data_name: 'e_94' }),
